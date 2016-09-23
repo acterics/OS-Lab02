b328bcbef2279e43940699043c53c3f8a69fa348
Oleg Lipskiy <lolego1601@gmail.com>
2016-09-23 22:41:47 +0300

    Adding SpinLock class

--- src/com/dreamteam/lab02/Main.java
+++ src/com/dreamteam/lab02/Main.java
@@ -1,10 +1,9 @@
 package com.dreamteam.lab02;
 
-/**
- * Created by root on 23.09.16.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
 }
+
---
+++ src/com/dreamteam/lab02/SpinLock.java
@@ -0,0 +1,43 @@
+package com.dreamteam.lab02;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SpinLock
+{
+    private final AtomicReference<Thread> _lock = new AtomicReference<>(null);
+    private final Lock _unlock = new Lock();
+
+    public Lock lock()
+    {
+        Thread thread = Thread.currentThread();
+        while(true)
+        {
+            if (!_lock.compareAndSet(null,thread))
+            {
+                if (_lock.get()==thread)
+                    throw new IllegalStateException("SpinLock is not reentrant");
+                continue;
+            }
+            return _unlock;
+        }
+    }
+
+    public boolean isLocked()
+    {
+        return _lock.get()!=null;
+    }
+
+    public boolean isLockedThread()
+    {
+        return _lock.get()==Thread.currentThread();
+    }
+
+    public class Lock implements AutoCloseable
+    {
+        @Override
+        public void close()
+        {
+            _lock.set(null);
+        }
+    }
+}
