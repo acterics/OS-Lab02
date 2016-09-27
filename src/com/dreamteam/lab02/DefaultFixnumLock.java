ba4d39a9a3e7cf0788d37323ed2dd74c1034059d
Nckcol <nckcol@gmail.com>
2016-09-27 12:39:40 +0300

    Default FixnumLock

--- src/com/dreamteam/lab02/DefaultFixnumLock.java
+++ src/com/dreamteam/lab02/DefaultFixnumLock.java
@@ -1,24 +1,40 @@
 package com.dreamteam.lab02;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
 
 abstract class DefaultFixnumLock implements FixnumLock {
 
     static int threads = 10;
-    static ArrayList<Integer> pidList;
+    static ArrayList<Integer> pidPool;
     int pid;
 
+    public DefaultFixnumLock() {
+        if(pidPool.size() < threads) return;
+
+        for(int i = pidPool.size(); i < threads; i++) {
+            pidPool.add(-1);
+        }
+    }
+
     public int getId() {
+        return pid;
+    }
+
+    private int getFree() {
+        for(int i = 0; i < pidPool.size(); i++) {
+            if(pidPool.get(i).equals(0)) return i;
+        }
+
         return 0;
     }
 
     public void register() {
-
+        pid = getFree();
+        pidPool.set(pid, pid);
     }
 
     public void unregister() {
-
+        pidPool.set(pid, -1);
+        pid = 0;
     }
 }
